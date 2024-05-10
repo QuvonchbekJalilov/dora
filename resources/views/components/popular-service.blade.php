6995985be84cf826ddcf4bca1daba279d5ab3191
Suhrob7472 <baxtiyorov.suhrob.2002@gmail.com>
2024-05-11 00:01:53 +0500

    11.05.2024 Suhrob

---
+++ resources/views/components/popular-service.blade.php
@@ -0,0 +1,94 @@
+<!-- rts-product area start -->
+<div class="rts-product-area rts-section-gap-2 pl_sm--10 pr_sm--10" id="product">
+        <div class="container-2">
+            <div class="row g-5 justify-content-between">
+                <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-12">
+                    <!-- single product area -->
+
+                    
+                    <div class="rts-product-area">
+                        <a class="thumbnail" href="#" data-sal-delay="150" data-sal="zoom-in" data-sal-duration="800">
+                            <img class="img-1" src="{{ asset('assets/images/popular_service/kafil-website.png') }}" alt="axela_product">
+
+                        </a>
+                        <div class="inner" data-sal-delay="250" data-sal="slide-up" data-sal-duration="1000">
+                            <span>DORA DEV.</span>
+                            <a href="#">
+                                <h5 class="title mb--0">
+                                @lang('index.popular_service.corporate_website')
+                                </h5>
+                            </a>
+                        </div>
+                    </div>
+
+                    <!-- single product area End -->
+                </div>
+                <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-12">
+                    <!-- single product area -->
+                    <div class="rts-product-area inner">
+                        <div class="wrapper">
+                            <a class="thumbnail" href="#" data-sal-delay="150" data-sal="zoom-in" data-sal-duration="800">
+                                <img class="img-1" src="{{ asset('assets/images/popular_service/create-logo.png') }}" alt="axela_product">
+
+                            </a>
+                            <div class="inner" data-sal-delay="350" data-sal="slide-up" data-sal-duration="1000">
+                                <span>DORA BRANDING</span>
+                                <a href="#">
+                                    <h5 class="title">   
+                                        @lang('index.popular_service.create_logo')              
+                                    </h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- single product area End -->
+                </div>
+            </div>
+            <div class="row g-5 justify-content-between pt--120 pt_md--80 pt_sm--60">
+                <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-12">
+                    <!-- single product area -->
+                    <div class="rts-product-area">
+                        <a class="thumbnail" href="#" data-sal-delay="150" data-sal="zoom-in" data-sal-duration="800">
+                            <img class="img-1" src="{{ asset('assets/images/popular_service/crm.png') }}" alt="axela_product">
+
+                        </a>
+                        <div class="inner" data-sal-delay="250" data-sal="slide-up" data-sal-duration="1000">
+                            <span>DORA DEV.</span>
+                            <a href="#">
+                                <h5 class="title mb--0">
+                                    @lang('index.popular_service.add_crm')  
+                                </h5>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- single product area End -->
+                </div>
+                <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-12">
+                    <!-- single product area -->
+                    <div class="rts-product-area inner">
+                        <div class="wrapper">
+                            <a class="thumbnail" href="#" data-sal-delay="150" data-sal="zoom-in" data-sal-duration="800">
+                                <img class="img-1" src="{{ asset('assets/images/popular_service/smm.png') }}" alt="axela_product">
+
+                            </a>
+                            <div class="inner" data-sal-delay="350" data-sal="slide-up" data-sal-duration="1000">
+                                <span>DORA MARKETING</span>
+                                <a href="#">
+                                    <h5 class="title">
+                                        @lang('index.popular_service.smm_sevice')  
+                                    </h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- single product area End -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- rts-product area end -->
+    <style>
+        .rts-product-area .inner .title{
+            text-transform: none;
+        }
+    </style>
